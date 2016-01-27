0273ae8f6215dec12f28862d47e79c29f6e57928
Mauricio Andrades <mod.and.op@gmail.com>
2016-01-26 22:05:06 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoRedditCloneSassApp
+ * @description
+ * # yoRedditCloneSassApp
+ *
+ * Main module of the application.
+ */
+angular.module('yoRedditCloneSassApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoRedditCloneSassApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoRedditCloneSassApp
+ */
+angular.module('yoRedditCloneSassApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
