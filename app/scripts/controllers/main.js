4c9a0b1cc104e84721635f5b71178c4ecad6a130
Mauricio Andrades <mod.and.op@gmail.com>
2016-01-28 03:07:15 -0800

    added post controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name RedditClone.controller:MainCtrl
@@ -8,10 +7,29 @@
  * Controller of the RedditClone
  */
 angular.module('RedditClone')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function($scope, $firebase, Posts) {
+      $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+      /* scope for posting things*/
+      $scope.posts = Posts;
+      /* save post function */
+      $scope.savePost = function(post) {
+        if (post.name && post.description && post.url && $scope.authData) {
+          //Actually adding the posts to the Firebase
+          Posts.$add({
+            //Setting the post name
+            name: post.name,
+            //Setting the post description
+            description: post.description,
+            //Setting the post URL
+            url: post.url,
+            //Setting the post votes
+            votes: 0,
+            //Getting the current user
+            user: $scope.authData.twitter.username
+          });
+        }
+      });
