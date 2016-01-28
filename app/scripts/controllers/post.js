57a210b612ce40d5ec32060099106e8ca336a901
Mauricio Andrades <mod.and.op@gmail.com>
2016-01-27 16:21:27 -0800

    was killing it, then 2 idiots came into the quiet room i was in and i had to come back into this terrible work enviroment where. Killed my concentration and completely derailed my transfer of the code I wrote and was splitting up to deploy and template from firebase

---
+++ app/scripts/controllers/post.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name RedditClone.controller:PostCtrl
+ * @description
+ * # PostCtrl
+ * Controller of the RedditClone
+ */
+angular.module('RedditClone')
+  .controller('PostCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -38,10 +38,10 @@ angular.module('RedditClone')
  * forcing a redirect to the /login page
  */
   .config(['$routeProvider', 'SECURED_ROUTES', function($routeProvider, SECURED_ROUTES) {
-    // credits for this idea: https://groups.google.com/forum/#!msg/angular/dPr9BpIZID0/MgWVluo_Tg8J
-    // unfortunately, a decorator cannot be use here because they are not applied until after
-    // the .config calls resolve, so they can't be used during route configuration, so we have
-    // to hack it directly onto the $routeProvider object
+    /*credits for this idea: https://groups.google.com/forum/#!msg/angular/dPr9BpIZID0/MgWVluo_Tg8J
+    unfortunately, a decorator cannot be use here because they are not applied until after
+    the .config calls resolve, so they can't be used during route configuration, so we have
+    to hack it directly onto the $routeProvider object*/
     $routeProvider.whenAuthenticated = function(path, route) {
       route.resolve = route.resolve || {};
       route.resolve.user = ['Auth', function(Auth) {
@@ -53,31 +53,26 @@ angular.module('RedditClone')
     };
   }])
 
-  // configure views; whenAuthenticated adds a resolve method to ensure users authenticate
-  // before trying to access that route
+  /*configure views; whenAuthenticated adds a resolve method to ensure users authenticate
+  before trying to access that route*/
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-
-      // .when('/chat', {
-      //   templateUrl: 'views/chat.html',
-      //   controller: 'ChatCtrl'
-      // })
-      // .when('/login', {
-      //   templateUrl: 'views/login.html',
-      //   controller: 'LoginCtrl'
-      // })
-      // .whenAuthenticated('/account', {
-      //   templateUrl: 'views/account.html',
-      //   controller: 'AccountCtrl'
-      // })
-      // .when('/postpic', {
-      //   templateUrl: 'views/postpic.html',
-      //   controller: 'PostpicCtrl'
-      // })
+      /*.when('/chat', {
+        templateUrl: 'views/chat.html',
+        controller: 'ChatCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .whenAuthenticated('/account', {
+        templateUrl: 'views/account.html',
+        controller: 'AccountCtrl'
+      })*/
       .otherwise({redirectTo: '/'});
   }])
 
@@ -89,11 +84,7 @@ angular.module('RedditClone')
    */
   .run(['$rootScope', '$location', 'Auth', 'SECURED_ROUTES', 'loginRedirectPath',
     function($rootScope, $location, Auth, SECURED_ROUTES, loginRedirectPath) {
-      // watch for login status changes and redirect if appropriate
       Auth.$onAuth(check);
-
-      // some of our routes may reject resolve promises with the special {authRequired: true} error
-      // this redirects to the login page whenever that is encountered
       $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
         if( err === 'AUTH_REQUIRED' ) {
           $location.path(loginRedirectPath);
@@ -114,3 +105,5 @@ angular.module('RedditClone')
 
   // used by route security
   .constant('SECURED_ROUTES', {});
+
+
